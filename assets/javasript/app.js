847f20d8af7d25c5773a4e8cc978078e4941bd04
Joshua Duncan <jduncan97@gmail.com>
2020-04-05 16:35:52 -0600

    Added html and set up initial script links

---
+++ assets/javasript/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCOzXzJ81Jxvg5qGdMyjPtDWJDifHSyjo4",
+    authDomain: "jduncan.firebaseapp.com",
+    databaseURL: "https://jduncan.firebaseio.com",
+    projectId: "jduncan",
+    storageBucket: "jduncan.appspot.com",
+    messagingSenderId: "189053086767",
+    appId: "1:189053086767:web:56ce8e3fae0e97f649aada",
+    measurementId: "G-NGMDBZT5H0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
