e777af59efa3b7f9511843610716ba7ac3e6e2c6
Joshua Duncan <jduncan97@gmail.com>
2020-04-06 14:18:02 -0600

    cut out some unneeded code

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -14,23 +14,6 @@ var firebaseConfig = {
   //Assign the database to a variable
   var database = firebase.database();
 
-  //variables to hold train data
-  // var trainName = $("#Input1").val().trim();
-  // var trainDestination = $("#Input2").val().trim();
-  // var firstTrain = $("#Input3").val().trim();
-  // var trainFrequency = $("#Input4").val().trim()
-
-  // var train = trainName;
-  // var destination = trainDestination;
-  // var firstArrival = firstTrain
-  // var frequency = trainFrequency
-
-
-  //On load take variable and update page or anytime database changes
-database.ref().on("value", function(snapshot) {
-
-});
-
 //When submit button is clicked 
 $("#submit-button").on("click", function(event) {
   event.preventDefault();
@@ -45,14 +28,16 @@ $("#submit-button").on("click", function(event) {
     Destination: trainDestination,
     Arrival: firstTrain,
     Frequency: trainFrequency
-  })
-  // $("#Input1").val("");
-  // $("#Input2").val("");
-  // $("#Input3").val("");
-  // $("#Input4").val("");
+  });
 
+  $("#Input1").val("");
+  $("#Input2").val("");
+  $("#Input3").val("");
+  $("#Input4").val("");
 
 });
+//need to add input data to table
+
 
 
   
