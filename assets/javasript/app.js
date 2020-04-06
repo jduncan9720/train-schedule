e552c20d717cb2ee53f7aaee66a0e55171c06f8c
Joshua Duncan <jduncan97@gmail.com>
2020-04-06 14:00:36 -0600

    Workign on JS

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -11,12 +11,51 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  //Assign the database to a variable
+  var database = firebase.database();
+
   //variables to hold train data
-  var trainName = "";
-  var destination = "";
-  var firstTime = "";
-  var frequency = 0;
-  var 
+  // var trainName = $("#Input1").val().trim();
+  // var trainDestination = $("#Input2").val().trim();
+  // var firstTrain = $("#Input3").val().trim();
+  // var trainFrequency = $("#Input4").val().trim()
+
+  // var train = trainName;
+  // var destination = trainDestination;
+  // var firstArrival = firstTrain
+  // var frequency = trainFrequency
+
+
+  //On load take variable and update page or anytime database changes
+database.ref().on("value", function(snapshot) {
+
+});
+
+//When submit button is clicked 
+$("#submit-button").on("click", function(event) {
+  event.preventDefault();
+
+  var trainName = $("#Input1").val().trim();
+  var trainDestination = $("#Input2").val().trim();
+  var firstTrain = $("#Input3").val().trim();
+  var trainFrequency = $("#Input4").val().trim()
+
+  database.ref("/trains").push({
+    Train: trainName,
+    Destination: trainDestination,
+    Arrival: firstTrain,
+    Frequency: trainFrequency
+  })
+  // $("#Input1").val("");
+  // $("#Input2").val("");
+  // $("#Input3").val("");
+  // $("#Input4").val("");
+
+
+});
+
+
+  
 
   
 
