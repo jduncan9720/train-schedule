af16fa4fa9a6703f9c2af28c428b5ca9d531ba84
Joshua Duncan <jduncan97@gmail.com>
2020-04-06 16:00:19 -0600

    more JS

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -7,12 +7,13 @@ var firebaseConfig = {
     messagingSenderId: "189053086767",
     appId: "1:189053086767:web:56ce8e3fae0e97f649aada",
     measurementId: "G-NGMDBZT5H0"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
 
-  //Assign the database to a variable
-  var database = firebase.database();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Assign the database to a variable
+var database = firebase.database();
 
 //When submit button is clicked 
 $("#submit-button").on("click", function(event) {
@@ -36,7 +37,23 @@ $("#submit-button").on("click", function(event) {
   $("#Input4").val("");
 
 });
+
 //need to add input data to table
+database.ref().on("child_added", function(snapshot) {
+
+  console.log("child_added");
+  var trainName = snapshot.val().Train;
+  var trainDestination = snapshot.val().Destination;
+  var firstTrain = snapshot.val().Arrival;
+  var trainFrequency = snapshot.val().Frequency;
+
+  // .append() to the table row to add a new row with trainName
+  $("#trainRows").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + firstTrain + "</td><td>" + trainFrequency + "</td></tr>");
+});
+
+
+
+
 
 
 
